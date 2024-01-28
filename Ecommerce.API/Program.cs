51cc4e12fe4016de1f5d34d95e5bb36aa42cad64
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-01-28 20:21:27 +0700

    Configure API running along with Blazor Client

--- Ecommerce.API/Program.cs
+++ Ecommerce.API/Program.cs
@@ -2,10 +2,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
@@ -14,12 +16,16 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseWebAssemblyDebugging();
 }
 
 app.UseHttpsRedirection();
-
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
 app.UseAuthorization();
 
+app.MapRazorPages();
 app.MapControllers();
+app.MapFallbackToFile("index.html");
 
 app.Run();
